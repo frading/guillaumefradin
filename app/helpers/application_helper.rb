1528a0aeac4d367c4f79bce2de2dbb37d00afa05
Guillaume Fradin <guillaumefradin@gmail.com>
2010-10-22 08:14:50 +0200

    added footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-	def link_to_custom(label, path)
-		render :partial=>'layouts/current_page', :locals=>{:label=>label, :path=>path}
+	def link_to_custom(label, path, html_class="")
+		render :partial=>'layouts/current_page', :locals=>{:label=>label, :path=>path, :html_class=>html_class}
 	end
 	
 end
