e65a0a69664101c06eb8c884f22f8d3ed879bff6
Guillaume Fradin <guillaumefradin@gmail.com>
2010-10-23 20:21:33 +0200

    added a redirected in case given heroku url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_uri
+  
+  protected
+  
+	def check_uri
+		return if request.local?
+		
+		base_host = "www.guillaumefradin.com"
+		#base_host = "everytimehq.heroku.com"
+		if request.host != base_host
+			redirect_to request.protocol + base_host + request.request_uri
+		end
+	end
+
 end
