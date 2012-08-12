8a14c6e4eba88933ff6203f66bd89495b07e90a2
Guillaume Fradin <guillaumefradin@gmail.com>
2012-08-12 08:28:41 +0100

    re added tracker

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,26 @@
-class Post < ActiveRecord::Base
+# class Post < ActiveRecord::Base
 
-	def previous_post
-		Post.where("published_at < ?", published_at).last
-	end
-	def more_recent_post
-		Post.where("published_at > ?", published_at).first
-	end
+# 	def previous_post
+# 		Post.where("published_at < ?", published_at).last
+# 	end
+# 	def more_recent_post
+# 		Post.where("published_at > ?", published_at).first
+# 	end
 	
-	def partial
-		partial_name || title.downcase
-	end
+# 	def partial
+# 		partial_name || title.downcase
+# 	end
 	
-	def publish
-		self.published_at = Time.now
-		self.published = true
-		self.save
-	end
+# 	def publish
+# 		self.published_at = Time.now
+# 		self.published = true
+# 		self.save
+# 	end
 	
-	def to_param
-		CGI::escape title
-	end
+# 	def to_param
+# 		CGI::escape title
+# 	end
 	
-end
+# end
 
 
