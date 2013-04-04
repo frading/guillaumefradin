080ebf21158fd7bc57ae331ed769c0e33c1e3aa6
Guillaume Fradin <guillaumefradin@gmail.com>
2013-04-04 21:02:38 +1300

    added link to seed.pdf on dropbox

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,7 +1,7 @@
 class ScriptsController < ApplicationController
   
 	def seed
-		redirect_to "https://www.dropbox.com/s/wjbkx8xasxfru27/seeds.pdf"
+		redirect_to "https://dl.dropbox.com/s/wjbkx8xasxfru27/seeds.pdf"
 	end
 	
 end
